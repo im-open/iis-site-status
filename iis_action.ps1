14a8f831ff7d700594c69c6dfc8eb2a965b6bc63
Danielle Adams <danielle.adams3@extendhealth.com>
2022-10-25 15:25:11 -0600

    ARCH-1615 - Updating syntax for powershell

--- iis_action.ps1
+++ iis_action.ps1
@@ -39,10 +39,10 @@ $script_result = Invoke-Command -ComputerName $server `
 
 If ($script_result -and $script_result -eq 1) {
     Write-Output "IIS Site Status Error"
-    Write-Output "website-status=IIS Site Status Error" >> $GITHUB_OUTPUT
+    "website-status=IIS Site Status Error" >> $env:GITHUB_OUTPUT
     Exit 1
 }
 else {
     Write-Output "$display_action_past_tense."
-    Write-Output "website-status=$script_result" >> $GITHUB_OUTPUT
+    "website-status=$script_result" >> $env:GITHUB_OUTPUT
 }
